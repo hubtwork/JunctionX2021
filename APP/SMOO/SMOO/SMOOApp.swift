bfc744b058dce1f23714c8ee929eb4547f438ea8
Hubtwork <hubtwork@gmail.com>
2021-05-22 03:11:59 +0900

    app: Initialize Swift App

---
+++ APP/SMOO/SMOO/SMOOApp.swift
@@ -0,0 +1,17 @@
+//
+//  SMOOApp.swift
+//  SMOO
+//
+//  Created by 허재 on 2021/05/22.
+//
+
+import SwiftUI
+
+@main
+struct SMOOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
