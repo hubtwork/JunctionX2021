a091c0e36e00868b47c6a68f5bacb7615767cdd3
Hubtwork <hubtwork@gmail.com>
2021-05-22 03:11:59 +0900

    app: Initialize Swift App

---
+++ APP/SMOO/SMOO/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SMOO
+//
+//  Created by 허재 on 2021/05/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
