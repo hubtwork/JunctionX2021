dfaf2ebcb2a321557596cfc6c06987e860ab201a
Hubtwork <hubtwork@gmail.com>
2021-05-22 12:31:12 +0900

    feat/ui: Basic Concept for Chatroom

---
+++ APP/SMOO/SMOO/UI/Screens/Chat.swift
@@ -0,0 +1,31 @@
+//
+//  Chat.swift
+//  SMOO
+//
+//  Created by 허재 on 2021/05/22.
+//
+
+import SwiftUI
+
+struct Chat: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+extension Chat {
+    
+    var content: some View {
+        ZStack {
+            
+        }
+    }
+    
+    
+}
+
+struct Chat_Previews: PreviewProvider {
+    static var previews: some View {
+        Chat()
+    }
+}
